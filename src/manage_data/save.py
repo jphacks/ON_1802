154faf360c8de5a304bc7077537048c9e066e0eb
kagari <e175766@ie.u-ryukyu.ac.jp>
2018-10-27 22:57:16 +0900

    関数名を少し修正し、確認することもできるようにした

--- src/manage_data/save.py
+++ src/manage_data/save.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def _chenge_dict_for_pandas(task_dict):
+def _change_dict_for_pandas(task_dict):
     
     for key, data in task_dict.items():
         task_dict[key] = [data]
@@ -9,9 +9,19 @@ def _chenge_dict_for_pandas(task_dict):
 
 def data_to_csv(dict):
 
-    task_dict = _change_dict_to_pandas(dict)
     file_name = str(dict['user_id']) + '.csv'
+    task_dict = _change_dict_for_pandas(dict)
     df = pd.DataFrame(task_dict)
     df.to_csv(file_name)
 
     return 0
+
+if __name__ == '__main__':
+    task_dict = {
+        'user_id' : 1,
+        'task_name' : '洗濯物をたたむ',
+        'task_info' : '洗濯物が溜まってきたのでそろそろ畳まないといけない',
+        'time_limit' : {'year':2018, 'month':10, 'date':18}
+    }
+    
+    data_to_csv(task_dict)
